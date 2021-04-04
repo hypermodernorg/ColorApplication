090afac74b185f24a19ec3bc458f763c2c2684d8
Lander Compton <lander083077@gmail.com>
2021-04-04 17:35:48 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ColorApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
